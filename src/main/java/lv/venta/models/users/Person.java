2b52b40d0ae531478f4cd4a02a8002af8fe4d578
localuser <localuser@VEA.LV>
2023-05-19 11:23:48 +0300

    Add: model class for Person

---
+++ src/main/java/lv/venta/models/users/Person.java
@@ -0,0 +1,54 @@
+package lv.venta.models.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "user_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+//TODO pielikt @ToString, ja nepieciešanma
+public class Person {
+	
+	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Idp")//DB puse būs kolonna "idp" un būs kā auto increment PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idp;
+	
+	@Column(name = "Name")//DB puse būs kolonna title
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burtam jābūt lielajam")
+	@Size(min = 3, max = 15, message = "Jabūt vismaz 3 un ne vairāk kā 15 simboliem")
+	private String name;
+	
+	@Column(name = "Surname")//DB puse būs kolonna title
+	@NotNull
+	@Pattern(regexp = "[A-ZĒŪĪĻĶŠĀŽČŅ]{1}[a-zēūīļķšāžčņ\\ ]+", message = "Pirmajam burtam jābūt lielajam")
+	@Size(min = 3, max = 15, message = "Jabūt vismaz 3 un ne vairāk kā 15 simboliem")
+	private String surname;
+	
+	@Column(name = "Personcode")//DB puse būs kolonna title
+	//TODO apdomāt un pielietot risinājumu ārzemju studentiem un jaunajiem LV personas kodiem
+	@NotNull
+	@Pattern(regexp = "[0-9]{6}-[0-9]{5}", message = "Neatbilstošs personas kods")
+	@Size(min = 3, max = 15, message = "Jabūt vismaz 3 un ne vairāk kā 15 simboliem")
+	private String personcode;
+	
+	
+	
+	
+}
